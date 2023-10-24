fe8b0051e6399ea9fd4bc114ff75271e2783f58b
Stéphane Busiere <7332724-BusiereStephane@users.noreply.gitlab.com>
2023-10-24 10:36:07 +0200

    feat(custom template)add blog archive section

--- wordpress/wp-content/themes/Digivate-test/src/parts/global/site-footer.php
+++ wordpress/wp-content/themes/Digivate-test/src/parts/global/site-footer.php
@@ -19,8 +19,8 @@
 
 
     <div class="site-info gutters text-center">
-        <a target="_blank" href="<?php echo esc_url( 'https://ignition.press/' ); ?>"><?php printf( __(
-				'© %s Created by %s with Ignition. Proudly powered by %s', 'digivate-test' ), date( 'Y' ), 'Eric', 'WordPress' ); ?></a>
+        <a target="_blank" href="<?php echo esc_url( 'https://busiere-stephane-dev.com' ); ?>"><?php printf( __(
+				'© %s Created by %s with Ignition. Proudly powered by %s', 'digivate-test' ), date( 'Y' ), 'Stéphane Busiere', 'WordPress' ); ?></a>
     </div><!-- .site-info -->
 
 </div>
--- wordpress/wp-content/themes/Digivate-test/src/parts/post/hero-banner.php
+++ wordpress/wp-content/themes/Digivate-test/src/parts/post/hero-banner.php
@@ -20,3 +20,98 @@ $post_hero_banner_image = get_field( 'post_hero_banner_image' );
 		<img class="hero-banner-image" src="<?php echo esc_url( $post_hero_banner_image ); ?>" alt="">
 	</div>
 </div>
+
+<section  <?php ign_block_attrs( $block, 'realisation-section full-width' ); ?> style="background-color:<?php echo esc_attr( $background_color ); ?>"data-scroll-section>
+
+			<?php
+			$taxonomies = array( 'category' );
+			$terms      = get_terms( $taxonomies, $args );
+			$args       = array(
+				'type'       => 'post',
+				'hide_empty' => false,
+				'parent'     => 0,
+				'exclude'    => 1,
+			);
+			?>
+			  
+			<!-- <div class="filter-container">
+				<div class="filter-item all" data-category="all"><?php echo __( 'Toutes les réalisations', 'Dixeed-2023' ); ?></div>
+				<?php
+				if ( ! empty( $terms ) ) :
+					foreach ( $terms as $terms ) :
+						$cat_id = $terms->term_id;
+
+						if ( $cat_id == 1 ) {
+							continue;
+						}
+						?>
+				<div class="filter-item" data-category="<?php echo $terms->slug; ?>"><?php echo $terms->slug; ?></div>
+						<?php
+				endforeach;
+						endif;
+				?>
+
+			</div> -->
+			<?php
+			$paged        = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+			$realisations = new WP_Query(
+				array(
+					'post_type' => 'post',
+					'order_by'  => 'date',
+					'order'     => 'desc',
+					'paged'     => $paged,
+				)
+			);
+			?>
+			<div class="loader-container"></div>
+			<div class="realisations-wrapper">
+				<?php if ( $realisations->have_posts() ) : ?>
+					<?php
+					while ( $realisations->have_posts() ) :
+						$realisations->the_post();
+						$categories = get_the_category();
+						?>
+				<div class="realisations-container">
+					<div class="realisations-text-left">
+						<h4 class="realisations-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+						<div class="realisations-category-container">
+						<?php
+						foreach ( $categories as $category ) {
+							?>
+								<div class="realisations-category"><?php echo esc_html( $category->name ); ?></div>
+						<?php } ?>
+						</div>
+					</div>
+					<div class="realisations-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'full' ); ?></a></div>
+
+				</div>
+						<?php wp_reset_postdata(); ?>
+						<?php
+				endwhile;
+					endif;
+				?>
+			</div>
+				<section class="pager m-50">
+								<div class="row">
+									<div class="pagination-container">
+									<?php
+									$big = 999999999;
+
+									echo paginate_links(
+										array(
+											'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+											'format'    => '?paged=%#%',
+											'current'   => max( 1, get_query_var( 'paged' ) . '/' ),
+											'total'     => $realisations ->max_num_pages,
+											'prev_next' => true,
+											'prev_text' => '<span >← Previous</span>',
+											'next_text' => '<span >Next →</span>',
+										)
+									);
+
+
+									?>
+									</div>
+								</div>
+			</section>
+</section>
